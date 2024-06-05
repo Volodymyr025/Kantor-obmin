7a148c31ecb7126d46cdb1e5e978f993df8f587f
Volodymyr025 <Fudo2790@gmail.com>
2024-06-05 12:36:51 +0300

    wip

--- src/lib/conectToDB.ts
+++ src/lib/conectToDB.ts
@@ -1,13 +1,18 @@
+import mongoose from "mongoose";
+
 export const conectToDB = async (nameDB: string) => {
+  let dbName = nameDB;
+  if (nameDB.includes("Чортків")) {
+    dbName = "Chortkiv";
+  }
+  if (nameDB.includes("Тернопіль")) {
+    dbName = "Ternopil";
+  }
+  const url = `${process.env.mongoUrl}${dbName}?retryWrites=true&w=majority&appName=Kantor`;
   try {
-    const request = await fetch(`${process.env.apiHttp}/api/conectToDB`, {
-      method: "POST",
-      body: JSON.stringify(nameDB),
-    });
-    const req = await request.json();
-    return req;
+    await mongoose.connect(url);
+    return console.log({ message: `conected ${dbName}` });
   } catch (err) {
-    console.log("conected field");
-    throw new Error("Field to conected");
+    return console.log({ message: `Filed to conected ${dbName}` + err });
   }
 };
