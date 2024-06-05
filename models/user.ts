131571630bb74f135f46384b5e184ddf5767bf2d
Volodymyr025 <Fudo2790@gmail.com>
2024-06-05 13:01:45 +0300

    wip

--- models/user.ts
+++ models/user.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   userName: String,
   password: String,
+  department: [],
 });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
