706d664c562966ec5534c5b19c5ad0cf425a6ea5
Volodymyr025 <Fudo2790@gmail.com>
2024-06-20 17:39:00 +0300

    wip

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,32 +1,12 @@
 "use client";
 import Header from "@/ui/components/Header/Header";
 import TableList from "@/ui/components/Table/PayDeskTable";
-import { UserInfo } from "@/ui/context-store/userInfo";
+
 import { getLocal } from "@/ui/utils/getLocalStore";
 import { Box, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 export default function Report() {
-  const [report, setReport] = useState([]);
-  const department = getLocal("Department");
-  const getReportFromDB = async () => {
-    try {
-      const res = await fetch(`/api/report`, {
-        method: "PATCH",
-        body: JSON.stringify(department.toString()),
-      });
-      const data = await res.json();
-      const lastAdded = await data.slice(-1);
-      setReport(lastAdded);
-    } catch {
-      throw Error('"Field to get report from server');
-    }
-  };
-  useEffect(() => {
-    void (async () => {
-      await getReportFromDB();
-    })();
-  }, [setReport]);
   return (
     <>
       <Header />
